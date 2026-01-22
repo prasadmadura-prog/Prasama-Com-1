1a708ea0e619b623d610825012917672b4252d3f
prasadmadura-prog <prasadmadura@gmail.com>
2026-01-22 10:29:25 +0530

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push("cjs");
-/* Fix: Enable package exports for Firebase v9+ modular support to correctly resolve sub-modules like /app and /auth */
-defaultConfig.resolver.unstable_enablePackageExports = true;
-module.exports = defaultConfig;
